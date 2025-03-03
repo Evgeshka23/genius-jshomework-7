178fa532cf76432ebd6c92849c294cfa00118ffa
Evgeshka23 <143422172+Evgeshka23@users.noreply.github.com>
2025-03-03 22:30:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Task 1
+
+// const block = document.querySelector("#text");
+// const button = document.querySelector("#btn");
+
+// button.addEventListener("click", () => {
+//   block.classList.toggle("hidden");
+// });
+
+// Task 2
+
+// const button = document.querySelector("button");
+// button.addEventListener("click", function () {
+//   this.style.display = "none";
+// });
+
+// Task 3
+
+// const nav = document.querySelector(".nav");
+// const sublist = nav.querySelector("ul");
+
+// nav.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   sublist.classList.toggle("hidden");
+// });
